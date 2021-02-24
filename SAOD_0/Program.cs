198e4af61b7c29dfef3ea0f514a523712e283a2e
Veler Jevsky <43819843+Valetron@users.noreply.github.com>
2021-02-24 13:12:55 +0300

    Add files via upload

--- SAOD_0/Program.cs
+++ SAOD_0/Program.cs
@@ -4,14 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SAOD_0
+namespace SAOD_0 // передаем в стек пару координат x, y; получаем координаты и заполняем
 {
     class Program
     {
 
-        static void fill(int x, int y, int color, int d, int[,] array, int recursion_deepth)
+        public static MyStack<ValueTuple<int, int>> stack = new MyStack<ValueTuple<int, int>>();
+        public static int x;
+        public static int y;
+        public static int[,] matrix;
+
+        static void fill(ValueTuple<int, int> coords)
         {
-            if (x >= 0 && y >= 0 && x < array.GetLength(0) && y < array.GetLength(1) && array[x, y] == d)
+            stack.Push(coords);
+
+            while (! stack.is_empty)
+            {
+                ValueTuple<int, int> last = stack.Peek();
+                
+                stack.Pop();
+                if (matrix[last.Item1, last.Item2] == 0)
+                {
+                    matrix[last.Item1, last.Item2] = 2;
+                }
+                //matrix[last.Item1, last.Item2] = 2;
+
+                if (last.Item1 + 1 < x && matrix[last.Item1 + 1, last.Item2] == 0)
+                {
+                    coords = new ValueTuple<int, int>(last.Item1 + 1, last.Item2);
+                    stack.Push(coords);
+                }
+                if (last.Item1 - 1 >= 0 && matrix[last.Item1 - 1, last.Item2] == 0)
+                {
+                    coords = new ValueTuple<int, int>(last.Item1 - 1, last.Item2);
+                    stack.Push(coords);
+                }
+                if (last.Item2 + 1 < y && matrix[last.Item1, last.Item2 + 1] == 0)
+                {
+                    coords = new ValueTuple<int, int>(last.Item1, last.Item2 + 1);
+                    stack.Push(coords);
+                }
+                if (last.Item2 - 1 >= 0 && matrix[last.Item1, last.Item2 - 1] == 0)
+                {
+                    coords = new ValueTuple<int, int>(last.Item1, last.Item2 - 1);
+                    stack.Push(coords);
+                }
+            }
+            /*if (x >= 0 && y >= 0 && x < array.GetLength(0) && y < array.GetLength(1) && array[x, y] == d)
             {
                 array[x, y] = color;
                 recursion_deepth++;
@@ -19,22 +58,21 @@ namespace SAOD_0
                 fill(x, y + 1, color, d, array, recursion_deepth);
                 fill(x - 1, y, color, d, array, recursion_deepth);
                 fill(x, y - 1, color, d, array, recursion_deepth);
-            }
+            }*/
         }
 
         static void Main(string[] args)
         {
-            int x;
-            int y;
             int count_0 = 0;
             int count_1 = 0;
-            Stack<int> my_stack = new Stack<int>();
             Random p = new Random(); // if p<0.25 == 1 else 0
+            
             Console.Write("X >> ");
             x = Convert.ToInt32(Console.ReadLine());
             Console.Write("Y >> ");
             y = Convert.ToInt32(Console.ReadLine());
-            int[,] matrix = new int[x, y];
+            matrix = new int[x, y];
+
             for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < y; j++)
@@ -54,10 +92,13 @@ namespace SAOD_0
                 }
                 Console.WriteLine();
             }
+            
             Console.WriteLine("One - " + count_1);
             Console.WriteLine("Zero - " + count_0);
             Console.WriteLine("\nFilled");
-            fill(0, 0, 2, 0, matrix, 0);
+
+            fill( (0, 0) );
+
             for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < y; j++)
@@ -66,6 +107,7 @@ namespace SAOD_0
                 }
                 Console.WriteLine();
             }
+            Console.ReadLine();
         }
     }
 }
