d3b84f35a2907c4d3f25c845b7dccaa744f39bf5
Veler Jevsky <43819843+Valetron@users.noreply.github.com>
2021-04-21 02:34:38 +0300

    Бинарное дерево

---
+++ SAOD_4/BinaryTree.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SAOD_4
+{
+    class BinaryTree
+    {
+        public class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+
+        public Node root;
+        private int count = 0;
+
+        public BinaryTree()
+        {
+
+        }
+
+        public void Clear()
+        {
+            root.left = null;
+            root.right = null;
+            count = 0;
+        }
+
+        public void Add(int data)
+        {
+            Node newItem = new Node(data);
+            if (root == null)
+            {
+                root = newItem;
+                count++;
+            }
+            else
+            {
+                root = RecursiveInsert(root, newItem);
+                count++;
+            }
+        }
+
+        private Node RecursiveInsert(Node current, Node n)
+        {
+            if (current == null)
+            {
+                current = n;
+                return current;
+            }
+            else if (n.data < current.data)
+            {
+                current.left = RecursiveInsert(current.left, n);
+                current = balance_tree(current);
+            }
+            else if (n.data > current.data)
+            {
+                current.right = RecursiveInsert(current.right, n);
+                current = balance_tree(current);
+            }
+            return current;
+        }
+
+        private Node balance_tree(Node current)
+        {
+            int b_factor = balance_factor(current);
+            if (b_factor > 1)
+            {
+                if (balance_factor(current.left) > 0)
+                {
+                    current = RotateLL(current);
+                }
+                else
+                {
+                    current = RotateLR(current);
+                }
+            }
+            else if (b_factor < -1)
+            {
+                if (balance_factor(current.right) > 0)
+                {
+                    current = RotateRL(current);
+                }
+                else
+                {
+                    current = RotateRR(current);
+                }
+            }
+            return current;
+        }
+
+        public void Delete(int target)
+        {
+            root = Delete(root, target);
+        }
+
+        private Node Delete(Node current, int target)
+        {
+            Node parent;
+            if (current == null)
+            { return null; }
+            else
+            {
+                if (target < current.data)
+                {
+                    current.left = Delete(current.left, target);
+                    if (balance_factor(current) == -2)//here
+                    {
+                        if (balance_factor(current.right) <= 0)
+                        {
+                            current = RotateRR(current);
+                        }
+                        else
+                        {
+                            current = RotateRL(current);
+                        }
+                    }
+                }
+                else if (target > current.data)
+                {
+                    current.right = Delete(current.right, target);
+                    if (balance_factor(current) == 2)
+                    {
+                        if (balance_factor(current.left) >= 0)
+                        {
+                            current = RotateLL(current);
+                        }
+                        else
+                        {
+                            current = RotateLR(current);
+                        }
+                    }
+                }
+                else
+                {
+                    if (current.right != null)
+                    {
+                        parent = current.right;
+                        while (parent.left != null)
+                        {
+                            parent = parent.left;
+                        }
+                        current.data = parent.data;
+                        current.right = Delete(current.right, parent.data);
+                        if (balance_factor(current) == 2)
+                        {
+                            if (balance_factor(current.left) >= 0)
+                            {
+                                current = RotateLL(current);
+                            }
+                            else { current = RotateLR(current); }
+                        }
+                    }
+                    else
+                    {
+                        return current.left;
+                    }
+                }
+            }
+            return current;
+        }
+
+        public void Find(int key)
+        {
+            if (Find(key, root).data == key)
+            {
+                Console.WriteLine("{0} was found!", key);
+            }
+            else
+            {
+                Console.WriteLine("Nothing found!");
+            }
+        }
+
+        private Node Find(int target, Node current)
+        {
+
+            if (target < current.data)
+            {
+                if (target == current.data)
+                {
+                    return current;
+                }
+                else
+                    return Find(target, current.left);
+            }
+            else
+            {
+                if (target == current.data)
+                {
+                    return current;
+                }
+                else
+                    return Find(target, current.right);
+            }
+
+        }
+
+        public void DisplayTree()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            InOrderDisplayTree(root);
+            Console.WriteLine();
+        }
+
+        private void InOrderDisplayTree(Node current)
+        {
+            if (current != null)
+            {
+                InOrderDisplayTree(current.left);
+                Console.Write("({0}) ", current.data);
+                InOrderDisplayTree(current.right);
+            }
+        }
+
+        private int max(int l, int r)
+        {
+            return l > r ? l : r;
+        }
+
+        public int getHeight(Node current)
+        {
+            int height = 0;
+            if (current != null)
+            {
+                int l = getHeight(current.left);
+                int r = getHeight(current.right);
+                int m = max(l, r);
+                height = m + 1;
+            }
+            return height;
+        }
+
+        private int balance_factor(Node current)
+        {
+            int l = getHeight(current.left);
+            int r = getHeight(current.right);
+            int b_factor = l - r;
+            return b_factor;
+        }
+
+        public Node RotateRR(Node parent)
+        {
+            Node pivot = parent.right;
+            parent.right = pivot.left;
+            pivot.left = parent;
+            return pivot;
+        }
+
+        public Node RotateLL(Node parent)
+        {
+            Node pivot = parent.left;
+            parent.left = pivot.right;
+            pivot.right = parent;
+            return pivot;
+        }
+
+        public Node RotateLR(Node parent)
+        {
+            Node pivot = parent.left;
+            parent.left = RotateRR(pivot);
+            return RotateLL(parent);
+        }
+
+        public Node RotateRL(Node parent)
+        {
+            Node pivot = parent.right;
+            parent.right = RotateLL(pivot);
+            return RotateRR(parent);
+        }
+
+        public Node r_rotation(Node r)
+        {
+            var p = r.left;
+            r.left = p.right;
+            p.right = r;
+            return p;
+        }
+
+        public Node l_rotation(Node r)
+        {
+            var p = r.right;
+            r.right = p.left;
+            p.left = r;
+            return p;
+        }
+
+        public Node b_r_rotation(Node p)
+        {
+            var piv = p.left;
+            p.left = l_rotation(piv);
+            return r_rotation(p);
+        }
+
+        public Node b_l_rotation(Node p)
+        {
+            var piv = p.right;
+            p.right = r_rotation(piv);
+            return l_rotation(p);
+        }
+
+        private KeyValuePair<int, string> ToStringHelper(Node n)
+        {
+            if (n == null)
+                return new KeyValuePair<int, string>(1, "\n");
+
+            var left = ToStringHelper(n.left);
+            var right = ToStringHelper(n.right);
+
+            var objString = n.data.ToString();
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(' ', left.Key - 1);
+            stringBuilder.Append(objString);
+            stringBuilder.Append(' ', right.Key - 1);
+            stringBuilder.Append('\n');
+
+            var i = 0;
+            while (i * left.Key < left.Value.Length && i * right.Key < right.Value.Length)
+            {
+                stringBuilder.Append(left.Value, i * left.Key, left.Key - 1);
+                stringBuilder.Append(' ', objString.Length);
+                stringBuilder.Append(right.Value, i * right.Key, right.Key);
+                ++i;
+            }
+
+            while (i * left.Key < left.Value.Length)
+            {
+                stringBuilder.Append(left.Value, i * left.Key, left.Key - 1);
+                stringBuilder.Append(' ', objString.Length + right.Key - 1);
+                stringBuilder.Append('\n');
+
+                ++i;
+            }
+
+            while (i * right.Key < right.Value.Length)
+            {
+                stringBuilder.Append(' ', left.Key + objString.Length - 1);
+                stringBuilder.Append(right.Value, i * right.Key, right.Key);
+                ++i;
+            }
+            return new KeyValuePair<int, string>(left.Key + objString.Length + right.Key - 1, stringBuilder.ToString());
+        }
+
+        public string Print()
+        {
+            var res = ToStringHelper(root).Value;
+            return res;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public bool Contains(int value)
+        {
+            Node currentvalue = root;
+
+            while (currentvalue != null)
+            {
+                if (value == currentvalue.data)
+                {
+                    return true;
+                }
+                else if (value < currentvalue.data)
+                {
+                    currentvalue = currentvalue.left;
+                }
+                else
+                {
+                    currentvalue = currentvalue.right;
+                }
+            }
+            return false;
+        }
+
+        public int MaxItr()
+        {
+            Node currentNode = root;
+            while (currentNode.right != null)
+            {
+                currentNode = currentNode.right;
+            }
+            return currentNode.data;
+        }
+
+        public int MinItr()
+        {
+            Node currentNode = root;
+            while (currentNode.left != null)
+            {
+                currentNode = currentNode.left;
+            }
+            return currentNode.data;
+        }
+
+        public int LeafCount(Node node)
+        {
+            if (node.left == null && node.right == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return LeafCount(node.left) + LeafCount(node.right);
+            }
+        }
+    }
+}
---
+++ SAOD_4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SAOD_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+
+            // добавление элементов
+            tree.Add(11);
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(2);
+            tree.Add(22);
+            tree.Add(4);
+            Console.WriteLine("Count of elements = " + tree.Count);
+
+            Console.WriteLine("\nTree");
+            Console.WriteLine(tree.Print());
+
+            // высота дерева
+            Console.WriteLine("Height = " + tree.getHeight(tree.root));
+
+            // кол-во листьев
+            Console.WriteLine("Leaves: " + tree.LeafCount(tree.root));
+
+            // нахождение элемента в дереве
+            Console.Write("Element to find: ");
+            int element = Convert.ToInt32(Console.ReadLine());
+            if (tree.Contains(element))
+            {
+                Console.WriteLine("Contains!");
+            }
+            else
+            {
+                Console.WriteLine("No such element");
+            }
+
+            // максимальное значение
+            Console.WriteLine("Maximum = " + tree.MaxItr());
+
+            //минимальное значение
+            Console.WriteLine("Minimum = " + tree.MinItr());
+
+            /*Console.WriteLine(tree.Print());
+            tree.root = tree.r_rotation(tree.root);
+            Console.WriteLine(tree.Print());
+            tree.root = tree.l_rotation(tree.root);
+            Console.WriteLine(tree.Print());*/
+
+            // удалить все элементы
+            tree.Clear();
+            Console.WriteLine("tree is empty, elements = " + tree.Count);
+        }
+    }
+}
